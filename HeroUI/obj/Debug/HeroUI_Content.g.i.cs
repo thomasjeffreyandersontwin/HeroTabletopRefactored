7edd4210a64e96e64383b1c5747ef4a5e6c739ab
istiaq <istiaq.ahmed.023@gmail.com>
2017-04-18 01:06:55 +1000

    fixed untracked files

--- HeroUI/obj/Debug/HeroUI_Content.g.i.cs
+++ HeroUI/obj/Debug/HeroUI_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("module.herovirtualtabletop.dll")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("herosystemsengine/common/images/search.png")]
 
 
